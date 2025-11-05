6fcb51734ff4cad5f40627d2a869bbb9b40457d7
Renaolol <renanschmeier91@gmail.com>
2025-11-05 14:35:23 -0300

    Big changes
    Criação dos DataFrames, busca pela API do conformidade fácil, criação do xlsx com os produtos classificados

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
 import pandas as pd
 from time import sleep
 from pathlib import Path
-from dependencies import extract_data_excel
-excel = Path(r"C:\Users\gcont\OneDrive\Documentos\GitHub\Classificador_IA\Pasta1.xlsx")
+from dependencies import extract_data_excel, get_api_conformidade_facil
+import os
+from dotenv import load_dotenv
+import json
+from pathlib import Path
+
+load_dotenv()
+# -----------------------
+# Planilhas Excel
+excel = Path(r"C:\Users\gcont\OneDrive\Documentos\GitHub\Classificador_IA\Produtos.xlsx")
 lei_complementar = Path(r"C:\Users\gcont\OneDrive\Documentos\GitHub\Classificador_IA\Lei 214 NCMs - CBSs .xlsx")
+
+# -----------------------
+# Criação e manipulação dos DataFrames
 excel_df = extract_data_excel(excel)
 lei_df = extract_data_excel(lei_complementar)
 classificacao = pd.merge(excel_df,lei_df,"left",on="NCM")
-print(classificacao[["Nome Produto","NCM","PRODUTO","Tributação"]])
+classificados_limpo = classificacao.fillna("Não compreendido pela LC 214/2025")
+
+#------------------------
+# Caminho do certificado e senha para a busca na API do conformidade fácil
+certificado = r"C:\Users\gcont\OneDrive\Desktop\GCONT.pfx"
+senha = os.getenv("SENHA_CERTIFICADO")
+caminhos = Path(".")
+for arquivo in caminhos.glob("*"):
+    existe = True
+    if arquivo.is_file() and 'CclassTrib.txt' in arquivo.name:
+        existe = False
+        print ("arquivo CclassTrib.txt encontrado")
+if existe:
+    cclass = get_api_conformidade_facil(certificado,senha)
+    # -----------------------
+    # Transformação do Json em um txt contendo o Json
+    with open("CclassTrib.txt", "w", encoding="utf-8") as arquivo:
+        json.dump(cclass.json(),arquivo,indent=4,ensure_ascii=False)
+
+
+#classificados_limpo[["Descrição","NCM","PRODUTO","Tributação"]].to_excel("classificados_limpo.xlsx")
--- dependencies.py
+++ dependencies.py
@@ -1,5 +1,12 @@
 import pandas as pd
 from pathlib import Path
+import requests
+import os
+import base64
+from requests_pkcs12 import Pkcs12Adapter
+from pprint import pprint
+from dotenv import load_dotenv
+import json
 
 def extract_data_excel(data):
     """Extract data from excel"""
@@ -8,4 +15,12 @@ def extract_data_excel(data):
 
     return data_df
 
+def get_api_conformidade_facil(certificado,senha):
+    session = requests.Session()  
+    session.mount("https://",Pkcs12Adapter(pkcs12_filename=certificado,pkcs12_password=senha))
+    url = "https://cff.svrs.rs.gov.br/api/v1/consultas/classTrib"
+    #Parametros aceitos cst ou NomeCst
+    response = session.get(url)
+    #Utilizado pprint para uma visualização encadeada
+    return response
 
