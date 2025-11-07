e9a539192726065f0a7c7a66c40ce45ed40df0d7
Renaolol <renanschmeier91@gmail.com>
2025-11-07 16:28:40 -0300

    Create agente.py

---
+++ agente.py
@@ -0,0 +1,19 @@
+from agno.agent import Agent
+from agno.models.openai import OpenAIResponses
+from dotenv import load_dotenv
+from dependencies import extract_data_excel
+import os
+from pathlib import Path
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+lei_complementar = Path(r"C:\Users\gcont\OneDrive\Documentos\GitHub\Classificador_IA\Lei 214 NCMs - CBSs .xlsx")
+lei_complementar_df = extract_data_excel(lei_complementar)
+agent_leitor = Agent(model=OpenAIResponses(id="gpt-4o-mini",name="Agente Leitor de Dados",api_key=api_key),
+                     markdown=True, stream=True)
+
+pergunta = f"Extraia os dados deste arquivo: {lei_complementar_df}"
+
+agent_leitor.print_response(pergunta)
