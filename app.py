6c1c35393ac4576250b147e9a33c9cddfc4cab19
Renaolol <renanschmeier91@gmail.com>
2025-11-12 07:46:37 -0300

    Update Agente.py
    Vou tentar recriar os agentes

--- app.py
+++ app.py
@@ -23,31 +23,9 @@ cst_class_df["CST"]=cst_class_df["CST"].astype(str)
 cst_class_df["cClassTrib"]=cst_class_df["cClassTrib"].astype(str)
 classificacao = pd.merge(excel_df,lei_df,"left",on="NCM")
 classificados_limpo = classificacao.fillna("Não compreendido pela LC 214/2025")
-#print(classificados_limpo.head(15))
-#print (cst_class_df.head(15))
+
 #------------------------
 # Caminho do certificado e senha para a busca na API do conformidade fácil
 certificado = r"C:\Users\gcont\OneDrive\Desktop\GCONT.pfx"
 senha = os.getenv("SENHA_CERTIFICADO")
 caminhos = Path(".")
-
-# Crie o agente informando apenas o df de CST e, opcionalmente, o ramo padrão
-agente = make_agente_interpretativo(cst_class_df, ramo_padrao="Super Mercado")
-
-# Exemplo único: MAMINHA ANGUS
-print(classificar_descricao(
-    "MAMINHA ANGUS",
-    agente,
-    ramo=None,            # usa o ramo_padrao se None
-))
-
-# Classificar o DataFrame todo (ajuste o nome da coluna de descrição)
-resultado = classificar_df_por_interpretacao(
-    classificados_limpo,
-    agente,
-    descricao_col="Descricao",  # ou "Descrição"
-    ramo="Super Mercado",
-    termo_inicial=None,
-    max_linhas=50               # teste com poucas linhas primeiro
-)
-print(resultado.head(10))
