9156b82d45ca69f98519c76a637b2474f39790e3
Renaolol <renanschmeier91@gmail.com>
2025-11-13 08:29:58 -0300

    Update
    recriando a lógica do programa. vai funcionar de forma diferente agora

--- app.py
+++ app.py
@@ -7,25 +7,38 @@ from dotenv import load_dotenv
 import json
 from pathlib import Path
 from pprint import pprint
-from agente import make_agente_interpretativo, classificar_descricao, classificar_df_por_interpretacao
 load_dotenv()
 # -----------------------
 # Planilhas Excel
-excel = Path(r"C:\Users\gcont\OneDrive\Documentos\GitHub\Classificador_IA\Produtos.xlsx")
-lei_complementar = Path(r"C:\Users\gcont\OneDrive\Documentos\GitHub\Classificador_IA\Lei 214 NCMs - CBSs .xlsx")
-cst_cclass_excel = Path(r"C:\Users\gcont\OneDrive\Documentos\GitHub\Classificador_IA\CST_CclassTrib_Resumo.xlsx")
+produtos = Path(r"C:\Users\gcont\OneDrive\Documentos\GitHub\Classificador_IA\database\Produtos.xlsx")
+lei_complementar = Path(r"C:\Users\gcont\OneDrive\Documentos\GitHub\Classificador_IA\database\Lei 214 NCMs - CBSs .xlsx")
+cst_cclass_excel = Path(r"C:\Users\gcont\OneDrive\Documentos\GitHub\Classificador_IA\database\CST_cclass.xlsx")
 # -----------------------
 # Criação e manipulação dos DataFrames
-excel_df = extract_data_excel(excel)
+#produtos_df = extract_data_excel(produtos)
 lei_df = extract_data_excel(lei_complementar)
-cst_class_df = pd.read_excel(cst_cclass_excel, dtype={'CST':'string','cClassTrib':'string'})
-cst_class_df["CST"]=cst_class_df["CST"].astype(str)
-cst_class_df["cClassTrib"]=cst_class_df["cClassTrib"].astype(str)
-classificacao = pd.merge(excel_df,lei_df,"left",on="NCM")
-classificados_limpo = classificacao.fillna("Não compreendido pela LC 214/2025")
+cst_cclass_excel_df = extract_data_excel(cst_cclass_excel)
+produto = {'descricao':['CARNE MOIDA CONG.MARCON 1KG'],'NCM':['0202.30.00']}
 
-#------------------------
-# Caminho do certificado e senha para a busca na API do conformidade fácil
-certificado = r"C:\Users\gcont\OneDrive\Desktop\GCONT.pfx"
-senha = os.getenv("SENHA_CERTIFICADO")
-caminhos = Path(".")
+df_teste = pd.DataFrame(produto)
+df_teste["NCM"] = df_teste["NCM"].str.replace(".","")
+df_merged = pd.merge(df_teste,cst_cclass_excel_df,left_on="NCM",right_on="allowed_ncmlist")
+print (df_merged)
+# print (df_teste)
+# print (cst_cclass_excel_df["allowed_ncmlist"].head(20))
+
+for x in cst_cclass_excel_df['allowed_ncmlist']:
+    if isinstance(x, str):
+        if df_teste['NCM'] in x:
+            print(x)
+    else:
+        continue
+
+
+
+
+# #------------------------
+# # Caminho do certificado e senha para a busca na API do conformidade fácil
+# certificado = r"C:\Users\gcont\OneDrive\Desktop\GCONT.pfx"
+# senha = os.getenv("SENHA_CERTIFICADO")
+# caminhos = Path(".")
